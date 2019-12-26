97bc79e2becf41bb8bb40359c88e838aaa020ad1
ouryoutube <58931339+ouryoutube@users.noreply.github.com>
2019-12-26 17:09:00 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-      direction: 'right'
-    });
-  });
