73415e823ffcb03479a282378e753f4e53008e6d
ouryoutube <58931339+ouryoutube@users.noreply.github.com>
2019-12-26 17:04:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-$(document).ready(function(){
-    $('.action-btn').floatingActionButton();
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'right'
+    });
   });
